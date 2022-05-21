85e832406248fc713a8aa4fbf6db7a656d995f1a
ScorchedTrace <Songkhla0@gmail.com>
2022-05-22 01:36:30 +0700

    Cloud

---
+++ Thought Train/Assets/Cloud.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public GameObject vision;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // vision = GameObject.Find("CanvasVision");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("enter");
+        GameObject collisionGameObject = collision.gameObject;
+        if (collisionGameObject.tag == "Player")
+        {
+            vision.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        GameObject collisionGameObject = collision.gameObject;
+        if (collisionGameObject.tag == "Player")
+        {
+            vision.SetActive(false);
+        }
+    }
+}
